88610f6e41ef8f2ab742732a6616b7ec85dd425d
Carbonkid658 <137427794+Carbonkid658@users.noreply.github.com>
2025-12-11 11:52:16 -0600

    Inital Commit

---
+++ src/main/java/net/carbonkid/tbf/TheBlockeyFrontierClient.java
@@ -0,0 +1,11 @@
+package net.carbonkid.tbf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheBlockeyFrontierClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
